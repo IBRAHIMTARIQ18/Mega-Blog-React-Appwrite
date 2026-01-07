1bd3f1c6cccb7b03b60bd4d4e25670c0839bd537
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2026-01-07 07:56:43 +0500

    authentication setup - register user

---
+++ src/appwrite/auth.js
@@ -0,0 +1,38 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setProject(conf.appwriteProjectId)
+      .setEndpoint(conf.appwriteUrl);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const user = await this.account.create({
+        userId: ID.unique(),
+        email,
+        password,
+        name,
+      });
+      if (user) {
+        //call another method for directly login after registering
+        return this.login({ email, password });
+      } else {
+        return user;
+      }
+    } catch (error) {
+      throw error();
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
