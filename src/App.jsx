839aa73e168e4cfc76053cb22a7554fa3d681325
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2026-01-11 08:01:19 +0500

    store configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
 import "./App.css";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  return (
-    <>
-      <h1>this is a mega blog</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-amber-500">
+      TEST
+    </div>
+  ) : null;
 }
 
 export default App;
